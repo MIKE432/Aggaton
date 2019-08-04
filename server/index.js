2512a3a7544fce0c4a17c9745b0629f5b3455996
MIKE432 <michal.raszczuk.apusoft@gmail.com>
2019-08-04 16:55:11 +0200

    started up express and added some cofigurations to package.Json

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express'),
+      cors = require('cors'),
+      app = express(),
+      PORT = 9000
+
+app.use(cors());
+
+app.listen(PORT, () => {
+    console.log(`App is listening on port ${PORT}!`)
+})
+console.log(process.env.PORT)
