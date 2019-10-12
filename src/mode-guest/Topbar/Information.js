6351455edfb379a5f49aff7fce84d23a2ab39e8d
MIKE432 <mike@MacBook-Pro-Micha.local>
2019-10-12 20:23:07 +0200

    AGT-23

--- src/mode-guest/Topbar/Information.js
+++ src/mode-guest/Topbar/Information.js
@@ -4,9 +4,19 @@ import Styles from './Information.module.scss'
 const Information = (props) => {
     return (
         <div className={Styles.information}>
-            <div>{props.informationType}</div>
-            <div>{props.information}</div>
-            <div>{props.date}</div>
+            <div className={Styles.informationDetails} >
+                <div className={Styles.informationHeader}>
+                    <div>{props.information}</div>
+                </div>
+                <div className={Styles.informationBody}>
+                    <div>{props.details}</div>
+                </div>
+            </div>
+            <div className={Styles.dateAndType}>
+                <div>{props.informationType}</div>
+                <div className={Styles.informationDate}>{props.date}</div>
+            </div>
+
         </div>
     )
 }
--- src/mode-guest/Topbar/Notifications.js
+++ src/mode-guest/Topbar/Notifications.js
@@ -7,12 +7,18 @@ class Notifications extends React.Component {
         return(
             <>
                 <div className={Styles.notificationsContainer}>
-                    <h4>Informacje</h4>
+                    <h4 className={Styles.header}>Informacje</h4>
                     <div>
-                        <Information date='24-01-19' informationType='wazna' information='masdoajkaf afs'/>
-                        <Information date='24-01-19' informationType='wazna' information='masdoajkaf afs'/>
-                        <Information date='24-01-19' informationType='wazna' information='masdoajkaf afs'/>
-                        <Information date='24-01-19' informationType='wazna' information='masdoajkaf afs'/>
+                        <Information date='24-01-19' informationType='wazna' information='Informacja o raku' details='rak to istota...'/>
+                        <Information date='24-01-19' informationType='wazna' information='Informacja o smoku' details='smok to istota...'/>
+                        <Information date='24-01-19' informationType='wazna' information='Informacja o rybie' details='ryba to istota...'/>
+                        <Information date='24-01-19' informationType='wazna' information='Informacja o człowieku' details='człowiek to istota...'/>
+                        <Information date='24-01-19' informationType='wazna' information='Informacja o człowieku' details='człowiek to istota...'/>
+                        <Information date='24-01-19' informationType='wazna' information='Informacja o człowieku' details='człowiek to istota...'/>
+                        <Information date='24-01-19' informationType='wazna' information='Informacja o człowieku' details='człowiek to istota...'/>
+                        <Information date='24-01-19' informationType='wazna' information='Informacja o człowieku' details='człowiek to istota...'/>
+                        <Information date='24-01-19' informationType='wazna' information='Informacja o człowieku' details='człowiek to istota...'/>
+                        <Information date='24-01-19' informationType='wazna' information='Informacja o człowieku' details='człowiek to istota...'/>
                     </div>
                 </div>
             </>
