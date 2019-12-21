bbe62b6ba7194924e25f6bfd539f62ee01fae4ba
Chris <krzysztof.raszczuk@gmail.com>
2019-12-21 17:57:47 +0100

    Trochę namieszałem

--- src/mode-guest/landing-page/BannerSection.js
+++ src/mode-guest/landing-page/BannerSection.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import BannerHolder from './BannerHolder/BannerHolder';
 import Logo from './Logo/Logo';
-import Button from '../../core/ctrls/Button';
 import Styles from './BannerSection.module.scss';
 
 const BannerSection = () => 
-    <BannerHolder>
-        <Logo size='512'/>
-        <div className={Styles.buttonsContainer}>
-            <Button to='/signin' mode='lp' style={Styles.button}>Zarejstruj się</Button>
-            <Button to='/login' mode='lps' style={Styles.button}>Zaloguj się</Button>
-        </div>
-    </BannerHolder>
+    <div className={Styles.banner}>
+        <div className={Styles.title}>Numizmatyczny Atlas Świata</div>
+        <div className={Styles.subtitle}>Wszystkie numizmaty w jednym miejscu.<br />Zarejestruj się, przeglądaj, wyszukuj i pytaj ekspertów!<br /> Poznawaj zachwycający numizmatyczny świat!</div>
+    </div>
 
 export default BannerSection;
